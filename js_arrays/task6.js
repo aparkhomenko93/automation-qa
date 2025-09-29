e679b8b01257bcff893afc387d7260056057558e
Andrey Parkhomenko <>
2025-09-29 10:50:26 +0300

    HW 8.1. Arrays in JavaScript

--- js_arrays/task6.js
+++ js_arrays/task6.js
@@ -2,7 +2,7 @@
 const numbersList = [1,10,14,2,4,5,43,34];
 
 // Sorting of array
-const sortedNumbers = numbersList.sort((a, b) => a - b);
+const sortedNumbers = numbersList.slice().sort((a, b) => a - b);
 
 // Show result in console
 console.log(sortedNumbers);
